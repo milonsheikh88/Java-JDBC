519ac9a2e0a5490ddf8ddb1fccfcfae3ef94a37c
Milon Sheikh <milonsheikh88@gmail.com>
2023-09-26 23:26:11 +0600

    MySQL JDBC Connection Done.

--- src/main/java/com/milon/sheikh/JavaJdbc/App.java
+++ src/main/java/com/milon/sheikh/JavaJdbc/App.java
@@ -1,12 +1,20 @@
 package com.milon.sheikh.JavaJdbc;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.milon.sheikh.JavaJdbc.db_connection.MySQLJDBCUtil;
+
 /**
  * @author [MILON SHEIKH]
  */
 
 public class App {
     public static void main( String[] args ){
-    	System.out.println("Hello World");
-    	    	  
+        try (Connection conn = MySQLJDBCUtil.getConnection()) {
+            System.out.println(String.format("Connected to database %s " + "successfully.", conn.getCatalog()));
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }   	  
     }
 }
