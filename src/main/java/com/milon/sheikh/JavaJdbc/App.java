3e1169c325496ac68ec33aef89a95f0880c54ec9
Milon Sheikh <milonsheikh88@gmail.com>
2023-09-27 23:10:29 +0600

    Delete query Using Statement.

--- src/main/java/com/milon/sheikh/JavaJdbc/App.java
+++ src/main/java/com/milon/sheikh/JavaJdbc/App.java
@@ -12,9 +12,9 @@ public class App {
     public static void main( String[] args ){
     	  
    	 CandidateDao cd=new CandidateDao();
-     int id = cd.insertData("Milon2", "Lily", Date.valueOf("1980-01-04"),"bush.l@yahoo.com", "(408) 898-6666");
+     int count = cd.deleteData(137);
 
-     System.out.println(String.format("A new candidate with id %d has been inserted.",id));
+     System.out.println(String.format("A new candidate with id %d has been deleted.",count));
   
     }
 }
--- src/main/java/com/milon/sheikh/JavaJdbc/daos/CandidateDao.java
+++ src/main/java/com/milon/sheikh/JavaJdbc/daos/CandidateDao.java
@@ -12,45 +12,21 @@ import com.milon.sheikh.JavaJdbc.db_connection.MySQLJDBCUtil;
  * @author [MILON SHEIKH]
  */
 public class CandidateDao {
-	public int insertData(String firstName,String lastName,Date dob, String email, String phone) {
+	public int deleteData(int id) {
 		 ResultSet rs = null;
-		int candidateId  = 0;
+		int rowCount  = 0;
         try (Connection conn = MySQLJDBCUtil.getConnection()) {
             System.out.println(String.format("Connected to database %s " + "successfully.", conn.getCatalog()));
             
             /// Using Statement
-//            String insertSQL = "insert into candidates "
-//					+"(first_name,last_name,dob,phone,email) "
-//					+"values ('" + firstName + "','" + lastName + "','" + dob + "','" + phone + "','" + email + "')";
-//            Statement stmt  = conn.createStatement();
-//            int rowAffected  = stmt.executeUpdate(insertSQL,Statement.RETURN_GENERATED_KEYS);
- 
-            
-            /// Using PreparedStatement
-            String insertSQL = "INSERT INTO candidates(first_name,last_name,dob,phone,email) "
-                    + "VALUES(?,?,?,?,?)";
-            
-            PreparedStatement pstmt = conn.prepareStatement(insertSQL,Statement.RETURN_GENERATED_KEYS);
-            // set parameters for statement
-            pstmt.setString(1, firstName);
-            pstmt.setString(2, lastName);
-            pstmt.setDate(3, dob);
-            pstmt.setString(4, phone);
-            pstmt.setString(5, email);
-
-            int rowAffected = pstmt.executeUpdate();
-            
-            if(rowAffected == 1){
-                // get candidate id
-                rs = pstmt.getGeneratedKeys();
-                if(rs.next())
-                candidateId = rs.getInt(1);
-            }
-                  
+            String deleteSQL = "DELETE FROM candidates WHERE id = '" + id + "'";
+            Statement stmt  = conn.createStatement();
+            rowCount = stmt.executeUpdate(deleteSQL);
+  
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-		return candidateId;
+		return rowCount;
 	}
 
 }
